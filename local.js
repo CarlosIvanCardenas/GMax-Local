74966d493d0d48934e740c576b89b4574cd9f9a6
CarlosIvanCardenas <a00820062@itesm.mx>
2017-02-19 21:00:24 -0600

    Servo test

--- local.js
+++ local.js
@@ -1,12 +1,14 @@
 //const socket = require('socket.io-client')('http://webarm.azurewebsites.net/');
-const socket = require('socket.io-client')('http://localhost:3000/');
+const socket = require('socket.io-client')('http://localhost:3000');
 const five = require('johnny-five');
 
 let led = null;
+let servo = null;
 
 five.Board().on('ready', function() {
   console.log('Arduino is ready.');
   led = new five.Led(13);
+  servo = new five.Servo(10);
 });
 
 socket.on('connect', function (server) {
@@ -16,8 +18,21 @@ socket.on('connect', function (server) {
 
 socket.on('prender', function() {
     led.on();
+    console.log('prender');
 })
 
 socket.on('apagar', function() {
     led.off();
+    servo.stop();
+    console.log('apagar');
+})
+
+socket.on('girar', function() {
+    servo.sweep();
+    console.log('girar');
+})
+
+socket.on('slide', function(value) {
+    servo.to(value);
+    console.log('girar a %d', value);
 })
